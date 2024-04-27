26f2a4edc2d1ec11c26fb89289b95c26a8e3bbd9
Yakkateela Ajay <yakkateela.ajay@joshsoftware.com>
2024-04-28 00:17:55 +0530

    side nav bar work done

--- src/app/dashboard/components/sidenav-wrapper/sidenav-wrapper.component.ts
+++ src/app/dashboard/components/sidenav-wrapper/sidenav-wrapper.component.ts
@@ -7,7 +7,35 @@ import { Component } from '@angular/core';
 })
 export class SidenavWrapperComponent {
 
-  isExpanded: boolean = false;
+  isExpanded: boolean = true;
+
+  navList: any = [
+    {
+      navLink: "dashboard",
+      icon: "grid_view",
+      navName: "Dashboard"
+    },{
+      navLink: "user",
+      icon: "account_balance",
+      navName: "Standard Management"
+    },{
+      navLink: "user1",
+      icon: "supervisor_account",
+      navName: "Re-Train ML Mode"
+    },{
+      navLink: "user2",
+      icon: "supervisor_account",
+      navName: "Register Employee"
+    },{
+      navLink: "user3",
+      icon: "supervisor_account",
+      navName: "Authorization\nManagement"
+    },{
+      navLink: "user4",
+      icon: "menu",
+      navName: "DataSet Management"
+    },
+  ]
 
   constructor() {}
 
